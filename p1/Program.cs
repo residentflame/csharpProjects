2f2d299c75a98e5ac7c32f1c428ad7ba823d2b0e
residentflame <luffygamingmc@gmail.com>
2025-08-01 18:00:19 -0400

    Multiply three numbers
    This is project idea #6 from W3resource's C# basics excercises

---
+++ p1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Variable data types
+int num_a;
+int num_b;
+int num_c;
+
+
+Console.Write("Enter the first integer: ");
+num_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second integer: ");
+num_b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third integer: ");
+num_c = Convert.ToInt32(Console.ReadLine());
+Console.Write(num_a * num_b * num_c);
