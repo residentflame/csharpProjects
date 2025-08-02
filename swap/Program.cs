2b5201ba877b6a4bd8ac97b22832d63487d5f7ce
residentflame <luffygamingmc@gmail.com>
2025-08-02 16:51:11 -0400

    Swap project added
    This is a project that swaps two numbers and prints the output

---
+++ swap/Program.cs
@@ -0,0 +1,15 @@
+﻿// Variable declarations
+int firstNum;
+int secondNum;
+int temp;
+int firstSwap;
+
+Console.Write("Input the First Number ");
+firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the Second Number: ");
+secondNum = Convert.ToInt32(Console.ReadLine());
+
+temp = firstNum;
+firstNum = secondNum;
+secondNum = temp;
+Console.WriteLine($"First Number: {firstNum}, Second Number: {secondNum}.");
