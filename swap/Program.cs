626eca7eabe17ee4f8e269a0794af82cbd0fc9e5
residentflame <luffygamingmc@gmail.com>
2025-08-02 16:56:25 -0400

    Cleaned up the code

--- swap/Program.cs
+++ swap/Program.cs
@@ -2,13 +2,16 @@
 int firstNum;
 int secondNum;
 int temp;
-int firstSwap;
 
-Console.Write("Input the First Number ");
+// Prompt for the 1st number:
+Console.Write("Input the First Number: ");
 firstNum = Convert.ToInt32(Console.ReadLine());
+
+// Prompt for the 2nd number:
 Console.Write("Input the Second Number: ");
 secondNum = Convert.ToInt32(Console.ReadLine());
 
+// Conversions
 temp = firstNum;
 firstNum = secondNum;
 secondNum = temp;
