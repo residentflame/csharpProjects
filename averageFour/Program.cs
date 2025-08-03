d695b452cb0b8f0597ab878765efc4cd83f84199
residentflame <luffygamingmc@gmail.com>
2025-08-03 13:04:53 -0400

    Average program project completed

---
+++ averageFour/Program.cs
@@ -0,0 +1,33 @@
+﻿//
+int numbOne;
+int numbTwo;
+int numbThree;
+int numbFour;
+int numbSum;
+int numbAvg;
+
+// Inputs
+
+Console.Write("Please enter the first number: ");
+numbOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+numbTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the third number: ");
+numbThree = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the fourth number: ");
+numbFour = Convert.ToInt32(Console.ReadLine());
+
+
+// Operations
+
+numbSum = numbOne + numbTwo + numbThree + numbFour;
+
+numbAvg = numbSum / 4;
+
+
+// Printing 
+
+Console.WriteLine($"The average of {numbOne}, {numbTwo}, {numbThree}, {numbFour} is: {numbAvg}");
