ea18190af33953c3202f8d61f7b80a35fb26189e
residentflame <luffygamingmc@gmail.com>
2025-08-04 13:35:30 -0400

    Added temperature converting program

---
+++ tempConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+int celsius;
+double kelvin;
+double fahrenheit;
+
+Console.Write("Enter the amount of celsius: ");
+celsius = Convert.ToInt32(Console.ReadLine());
+
+kelvin = Math.Round(celsius + 273.15);
+fahrenheit = Math.Round((celsius * 1.8) + 32);
+Console.WriteLine($"Kelvin: {kelvin} ");
+Console.WriteLine($"Fahrenheit: {fahrenheit} ");
+
+
