625ddaec980503d72e1c9efd7cf1b928035c5954
residentflame <luffygamingmc@gmail.com>
2025-08-07 14:07:20 -0400

    Multiplication table

---
+++ multiplicationTable/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a number: ");
+        int userNum = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"This is the multiplication table of {userNum}\n");
+        int product;
+        for (int j =1; j <= 10; j++)
+        {
+            product = userNum * j;
+            Console.WriteLine($"{userNum} x {j}: {product}");
+        }
