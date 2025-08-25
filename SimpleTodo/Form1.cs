8aa575b9ef24083645ff6f306d49c10b455104e8
residentflame <luffygamingmc@gmail.com>
2025-08-24 22:53:41 -0400

    Added save list to a file

--- SimpleTodo/Form1.cs
+++ SimpleTodo/Form1.cs
@@ -1,5 +1,7 @@
 using System.Windows.Forms;
-
+using System.IO;
+using System.Collections;
+using System.Diagnostics.Eventing.Reader;
 namespace SimpleTodo
 {
     public partial class Form1 : Form
@@ -9,6 +11,8 @@ namespace SimpleTodo
             InitializeComponent();
         }
 
+
+        List<string> TodoItems = new List<string>();
         private void addTask_Click(object sender, EventArgs e)
         {
             // Checks if the text task is not null
@@ -16,7 +20,11 @@ namespace SimpleTodo
             {
                 // Assigns text box text (textInput) to the Listbox named taskBox
                 taskBox.Items.Add(taskInput.Text);
-                MessageBox.Show($"Task added successfully!");
+
+                // Adds text to the TodoItems list 
+                TodoItems.Add(taskInput.Text);
+
+                MessageBox.Show("Task added successfully!");
                 taskInput.Clear();
             }
             else
@@ -31,7 +39,7 @@ namespace SimpleTodo
         {
             // Checks if the list task is selected
             if (taskBox.SelectedItem != null)
-            {    
+            {   // remove selected ListBox item if != null
                 taskBox.Items.Remove(taskBox.SelectedItem);
                 MessageBox.Show("Item removed successfully!");
             }
@@ -42,5 +50,51 @@ namespace SimpleTodo
             taskInput.Focus();
         }
 
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the SaveFileDialog
+            SaveFileDialog SaveFileDialog = new SaveFileDialog();
+            SaveFileDialog.Filter = "Text File|*.txt";
+            // Show the dialog and check if the user clicked OK
+            if (SaveFileDialog.ShowDialog() == DialogResult.OK)
+
+            {
+                var filePath = SaveFileDialog.FileName;
+
+                // Check if the file path is not null
+                if (!string.IsNullOrEmpty(filePath))
+                {
+
+                    if (TodoItems != null && TodoItems.Count > 0)
+                    {
+                        using (StreamWriter writer = new StreamWriter(filePath, true))
+                        {
+                            // Loop through each item in the todoItems list
+                            foreach (string todoItem in TodoItems)
+                            {
+                                writer.WriteLine(todoItem);
+                            }
+                        }
+                        // Notify user the list has been saved
+                        MessageBox.Show("List saved to file successfully!");
+
+                        // Show the file save location
+                        MessageBox.Show($"File saved at: {filePath}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter at least one item to the list!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File location not valid!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No file location was selected. Please try again.");
+            }
+        }
     }
 }
